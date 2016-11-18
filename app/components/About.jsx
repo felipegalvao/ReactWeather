a367045a2a667868f8e8fb9b24c5d3ba468ff87d
Felipe Galvao <contato@felipegalvao.com.br>
2016-11-17 22:16:21 -0200

    Style About Page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -4,8 +4,20 @@ var About = (props) => {
   
     return (
       <div>
-        <h3>About Component</h3>
-        <p>This is the About Component.</p>
+        <h1 className="text-center">About</h1>
+        <p>This is a Weather Application built on React. I have 
+        built this for The Complete React Web App Developer Course.</p>
+        <p>Here are some of the tools I used:</p>
+        <ul>
+          <li>
+            <a href="https://facebook.github.io/react">React</a> - 
+            Javascript Framework
+          </li>
+          <li>
+            <a href="http://openweathermap.org">Open Weather Map</a> - 
+            API to search weather data by city name
+          </li>
+        </ul>
       </div>
     );
   }
